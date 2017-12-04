1fbcb0ce2e1b2ac3e9a5cd9d093fd5fefeda753b
Kerry Buckley <kerryjbuckley@gmail.com>
2017-12-04 20:32:54 +0000

    Remove boilerplate

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,17 +1,4 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for Dictionary.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello
-      :world
-
-  """
   def hello do
     :world
   end

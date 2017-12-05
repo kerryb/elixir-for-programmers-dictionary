5ef7fa936fb1c2c2b4e28ddb927a8b2e85c0d282
Kerry Buckley <kerryjbuckley@gmail.com>
2017-12-05 20:44:40 +0000

    Read word list and return a random word

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,5 +1,10 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts "Hello world"
+  def random_word do
+    Enum.random word_list()
+  end
+
+  def word_list do
+    contents = File.read! "assets/words.txt"
+    String.split contents, "\n"
   end
 end
